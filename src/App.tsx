d5072ac28fadaadf841cb51ad791329842114d15
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-12-31 19:46:12 -0600

    added failed to connect message

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import "./App.css";
 import { useDiceSet, useDieColor, useDieValue } from "go-dice-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [dice, requestDie] = useDiceSet();
+  const [addedDice, setAddedDice] = useState(false);
+  const [addedDiceFail, setAddedDiceFail] = useState(false);
   useEffect(() => {
     console.log(dice);
   }, [dice]);
 
+  useEffect(() => {
+    if (addedDice) {
+      dice.length === 0 ? setAddedDiceFail(true) : null;
+      return;
+    }
+
+    setAddedDiceFail(false);
+  });
+
   return (
     <div className="App">
       <div>
-        <button onClick={() => requestDie()}>Request Dice</button>
+        <button
+          onClick={() => {
+            setAddedDice(true);
+            requestDie();
+          }}
+        >
+          Request Dice
+        </button>
         {dice.map((die) => (
           <>
             <h1>Die Id: {die.id}</h1>
@@ -20,6 +38,7 @@ function App() {
           </>
         ))}
       </div>
+      {addedDiceFail ? <h3>Failed to connect to Dice</h3> : null}
     </div>
   );
 }
