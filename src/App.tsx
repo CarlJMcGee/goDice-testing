11d4b8c4ddc1a3d0a97697329378e37399b69ec4
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-01-08 21:04:38 -0600

    added select for die set type

--- src/App.tsx
+++ src/App.tsx
@@ -8,61 +8,42 @@ import GenesysSet from "./components/GenesysSet";
 
 function App() {
   const [dice, requestDie] = useDiceSet();
-  const [testDice, setDice] = useState<
-    {
-      id: string;
-      color: keyof typeof LedColor;
-      battery: number;
-      rolling: boolean;
-      value: number;
-    }[]
-  >([]);
-  useEffect(() => {
-    console.log(dice);
-  }, [dice]);
+  const [diceBox, openDiceBox] = useState<typeof diceBoxen[number]>("standard");
 
-  function createDiceHandler() {
-    const seeds = setNumBetween(3, 1, 100);
-    let dice: {
-      id: string;
-      color: keyof typeof LedColor;
-      battery: number;
-      rolling: boolean;
-      value: number;
-    }[] = [];
-
-    const colors = ["BLUE", "RED", "GREEN", "OFF"] as const;
-
-    seeds.forEach((seed) => {
-      dice.push({
-        id: `${seed}`,
-        color: colors[numBetween(0, 3)],
-        battery: numBetween(0, 100),
-        rolling: numBetween(1, 1000) % 2 === 0,
-        value: numBetween(1, 6),
-      });
-    });
-
-    setDice([...testDice, ...dice]);
-  }
+  const diceBoxen = ["standard", "genesys"] as const;
 
   return (
     <div className="App flex justify-center items-center min-h-screen">
       <div className="flex flex-col">
+        <select
+          className="text-blue-400 py-3 px-5 bg-gray-800 border-transparent border-2 rounded-md hover:border-cyan-400 hover:text-blue-200 hover:bg-slate-600"
+          onChange={(e) =>
+            openDiceBox(e.target.value as typeof diceBoxen[number])
+          }
+        >
+          {diceBoxen.map((box) => (
+            <option key={box} value={box}>
+              {box.toLocaleUpperCase()}
+            </option>
+          ))}
+        </select>
         <button
-          className="text-blue-400 my-3 px-5 py-3 rounded-md bg-gray-800 hover:border-2 hover:border-cyan-400 hover:text-blue-200 hover:bg-slate-600"
+          className="text-blue-400 my-3 px-5 py-3 border-2 border-transparent rounded-md bg-gray-800 hover:border-cyan-400 hover:text-blue-200 hover:bg-slate-600"
           onClick={() => {
             requestDie();
           }}
         >
           Request Dice
         </button>
-        <GenesysSet dice={dice} />
-        <div className="grid grid-cols-1 md:grid-cols-3">
-          {dice.map((die, i) => (
-            <DieDisplay key={die.id} die={die} index={i} />
-          ))}
-        </div>
+        {diceBox === "genesys" ? (
+          <GenesysSet dice={dice} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            {dice.map((die, i) => (
+              <DieDisplay key={die.id} die={die} index={i} />
+            ))}
+          </div>
+        )}{" "}
       </div>
     </div>
   );
